7aac5440e982a99b9a8a282146cfe82104d42b77
unknown <lukas.raila@ktu.edu>
2017-12-04 12:35:23 +0200

    Frontpage grid

--- server/client/src/components/Footer.js
+++ server/client/src/components/Footer.js
@@ -7,50 +7,15 @@ class Footer extends Component {
 
     render() {
         return (
-            // <footer className="page-footer deep-purple" style={{paddingBottomposition: "fixed", bottom: "0", width: "100%"}} >
-            //     <div className="container">
-            //         <div className="row">
-            //             <div className="col l6 s12">
-            //             <h5 className="white-text">Company Bio</h5>
-            //             <p className="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
-
-
-            //             </div>
-            //             <div className="col l3 s12">
-            //             <h5 className="white-text">Settings</h5>
-            //             <ul>
-            //                 <li><a className="white-text" href="#!">Link 1</a></li>
-            //                 <li><a className="white-text" href="#!">Link 2</a></li>
-            //                 <li><a className="white-text" href="#!">Link 3</a></li>
-            //                 <li><a className="white-text" href="#!">Link 4</a></li>
-            //             </ul>
-            //             </div>
-            //             <div className="col l3 s12">
-            //             <h5 className="white-text">Connect</h5>
-            //             <ul>
-            //                 <li><a className="white-text" href="#!">Link 1</a></li>
-            //                 <li><a className="white-text" href="#!">Link 2</a></li>
-            //                 <li><a className="white-text" href="#!">Link 3</a></li>
-            //                 <li><a className="white-text" href="#!">Link 4</a></li>
-            //             </ul>
-            //             </div>
-            //         </div>
-            //     </div>
-            //     <div className="footer-copyright">
-            //         <div className="container">
-            //             Made by <a className="orange-text text-lighten-3" href="http://materializecss.com">Materialize</a>
-            //         </div>
-            //     </div>               
-            // </footer>
-            <Segment
-                inverted
-                vertical
-                style={{ margin: '5em 0em 0em', padding: '5em 0em' }}
-            >
+          <Segment
+            inverted
+            vertical
+            style={{ margin: '0em 0em 0em', padding: '5em 0em' }}
+          >
             <Container textAlign='center'>
               <Grid divided inverted stackable>
                 <Grid.Row>
-                  <Grid.Column width={3}>
+                  <Grid.Column width={4}>
                     <Header inverted as='h4' content='Group 1' />
                     <List link inverted>
                       <List.Item as='a'>Link One</List.Item>
@@ -59,7 +24,7 @@ class Footer extends Component {
                       <List.Item as='a'>Link Four</List.Item>
                     </List>
                   </Grid.Column>
-                  <Grid.Column width={3}>
+                  <Grid.Column width={4}>
                     <Header inverted as='h4' content='Group 2' />
                     <List link inverted>
                       <List.Item as='a'>Link One</List.Item>
@@ -68,18 +33,9 @@ class Footer extends Component {
                       <List.Item as='a'>Link Four</List.Item>
                     </List>
                   </Grid.Column>
-                  <Grid.Column width={3}>
-                    <Header inverted as='h4' content='Group 3' />
-                    <List link inverted>
-                      <List.Item as='a'>Link One</List.Item>
-                      <List.Item as='a'>Link Two</List.Item>
-                      <List.Item as='a'>Link Three</List.Item>
-                      <List.Item as='a'>Link Four</List.Item>
-                    </List>
-                  </Grid.Column>
-                  <Grid.Column width={3}>
+                  <Grid.Column textAlign='center' width={6}>
                     <Header inverted as='h4' content='Footer Header' />
-                    <p>Extra space for a call to action inside the footer that could help re-engage users.</p>
+                    <p>We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
                   </Grid.Column>
                 </Grid.Row>
               </Grid>
--- server/client/src/components/Landing.js
+++ server/client/src/components/Landing.js
@@ -1,60 +1,121 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Footer from './Footer';
+import { Segment, Container, Grid,Header, List, Divider, Image, Button, Menu, Icon } from 'semantic-ui-react';
 
 const Landing = () => {
   return (
-    <div className="grey lighten-4">
-      <div className="section no-pad-bot" id="index-banner">
-        <div className="container">
-          <br/><br/>
-          <h1 className="header center text"> 4Scroll!</h1>
-          <div className="row center">
-            <h5 className="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
-          </div>
-          <div className="row center">
-          <Link to={"/feeds"}>
-            <span className="btn-large waves-effect waves-light red lighten-1">Get Started</span>
-          </Link>
-          </div>
-          <br/><br/>
-        </div>
-      </div>
+    <div>
+      <Segment
+            inverted
+            textAlign='center'
+            style={{ minHeight: 700, padding: '1em 0em' }}
+            vertical
+          >
+            {/* <Container>
+              <Menu inverted pointing secondary size='large'>
+                <Menu.Item as='a' active>Home</Menu.Item>
+                <Menu.Item as='a'>Work</Menu.Item>
+                <Menu.Item as='a'>Company</Menu.Item>
+                <Menu.Item as='a'>Careers</Menu.Item>
+                <Menu.Item position='right'>
+                  <Button as='a' inverted>Log in</Button>
+                  <Button as='a' inverted style={{ marginLeft: '0.5em' }}>Sign Up</Button>
+                </Menu.Item>
+              </Menu>
+            </Container> */}
 
+          <Container text>
+            <Header
+              as='h1'
+              content='4Scroll'
+              inverted
+              style={{ fontSize: '4em', fontWeight: 'normal', marginBottom: 0, marginTop: '3em' }}
+            />
+            <Header
+              as='h2'
+              content='A modern way to collect feeds!'
+              inverted
+              style={{ fontSize: '1.7em', fontWeight: 'normal' }}
+            />             
+            <Button primary size='huge' as={Link} to={"/feeds"}>
+              Get Started
+              <Icon name='right arrow' />
+            </Button>
+          </Container>
+        </Segment>
 
-      <div className="container">
-        <div className="section">
-          <div className="row">
-            <div className="col s12 m4 ">
-              <div className="icon-block">
-                <h2 className="center red-text lighten-1-text"><i className="material-icons">flash_on</i></h2>
-                <h5 className="center">Speeds up development</h5>
+        <Segment style={{ padding: '0em' }} vertical>
+          <Grid celled='internally' columns='equal' stackable>
+            <Grid.Row textAlign='center'>
+              <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
+              {/* <Grid.Column width={8}> */}
+              <Header as='h3' style={{ fontSize: '2em' }}>We Help Companies and Companions</Header>
+              <p style={{ fontSize: '1.33em' }}>
+                We can give your company superpowers to do things that they never thought possible. Let us delight
+                your customers and empower your needs... through pure data analytics.
+              </p>
+              
+            {/* </Grid.Column> */}
+              </Grid.Column>
+              <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
+              <Header as='h3' style={{ fontSize: '2em' }}>We Make Bananas That Can Dance</Header>
+                <p style={{ fontSize: '1.33em' }}>
+                  Yes that's right, you thought it was the stuff of dreams, but even bananas can be bioengineered.
+                </p>
+                </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Segment>
 
-                <p className="light">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components. Additionally, we refined animations and transitions to provide a smoother experience for developers.</p>
-              </div>
-            </div>
+        <Segment vertical style={{ padding: '5em 0em' }}>
+          <Container>
+            <Grid divided  stackable  columns='equal'>
+              <Grid.Row textAlign='center'>
+                <Grid.Column >
+                  <Header as='h3' style={{ fontSize: '1.5em' }}>Speeds up development</Header>
+                  <p>We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components. Additionally, we refined animations and transitions to provide a smoother experience for developers.</p>
+                </Grid.Column>
+                <Grid.Column >
+                  <Header  as='h3' content='User Experience Focused' style={{ fontSize: '1.5em' }}/>
+                  <p>By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>
+                </Grid.Column>
+                <Grid.Column >
+                  <Header as='h3' style={{ fontSize: '1.5em' }} >Easy to work with</Header>
+                  <p>We have provided detailed documentation as well as specific code examples to help new users get started. We are also always open to feedback and can answer any questions a user may have about Materialize.</p>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Container>
+        </Segment>
 
-            <div className="col s12 m4">
-              <div className="icon-block">
-                <h2 className="center red-text lighten-1-text"><i className="material-icons">group</i></h2>
-                <h5 className="center">User Experience Focused</h5>
-                <p className="light">By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>
-              </div>
-            </div>
-
-            <div className="col s12 m4">
-              <div className="icon-block">
-                <h2 className="center red-text lighten-1-text"><i className="material-icons">settings</i></h2>
-                <h5 className="center">Easy to work with</h5>
-                <p className="light">We have provided detailed documentation as well as specific code examples to help new users get started. We are also always open to feedback and can answer any questions a user may have about Materialize.</p>
-              </div>
-            </div>
-          </div>
-
-        </div>
-        <br/><br/>
-      </div>
-      <Footer />
+        <Segment style={{ padding: '8em 0em' }} vertical>
+          <Container text>
+            <Header as='h3' style={{ fontSize: '2em' }}>Breaking The Grid, Grabs Your Attention</Header>
+            <p style={{ fontSize: '1.33em' }}>
+              Instead of focusing on content creation and hard work, we have learned how to master the art of doing
+              nothing by providing massive amounts of whitespace and generic content that can seem massive, monolithic
+              and worth your attention.
+            </p>
+            <Button as='a' size='large'>Read More</Button>
+            <Divider
+              as='h4'
+              className='header'
+              horizontal
+              style={{ margin: '3em 0em', textTransform: 'uppercase' }}
+            >
+              <a href='#'>Case Studies</a>
+            </Divider>
+            <Header as='h3' style={{ fontSize: '2em' }}>Did We Tell You About Our Bananas?</Header>
+            <p style={{ fontSize: '1.33em' }}>
+              Yes I know you probably disregarded the earlier boasts as non-sequitur filler content, but it's really
+              true.
+              It took years of gene splicing and combinatory DNA research, but our bananas can really dance.
+            </p>
+            <Button as='a' size='large'>I'm Still Quite Interested</Button>
+          </Container>
+        </Segment>     
+        <Footer />
     </div>
 
   );
