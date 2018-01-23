2c6b5dc38e3a53bff193fc511aa893172d51d8af
lukrai <lukrai007@gmail.com>
2018-01-23 14:13:17 +0200

    Delete keys.js

--- server/config/keys.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    googleClientID: "1052695240939-r0doqk3gsk9i0e3abp1qu7ch4l61fdp5.apps.googleusercontent.com",
-    googleClientSecret: "2O29sCRbkiRlZw135j1cP0YG",
-    mongoURI: "mongodb://lukrai:lukaS987qq@ds038547.mlab.com:38547/4scroll",
-    cookieKey: '9ABEA72DF761FCADFG2DFC94579F33',
-    fbAccessKey: "EAAIXnWX1aikBAGZB5mLtu3EIHHhVCccPBGkavFbsiRPAXB8542D7TneYBpGuB6mfVlbpYmWNYHcxZAVLnncnZCMZA2V1gZCG08CeNVx7kwa6KYg5jsP2GqDiNPvxpBdw0RGbIJDSAWirPUn46ifxwwkFssXfYI9qrk4HuLPZCH3QZDZD"
-};
